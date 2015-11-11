0ab89b2a92c218a66619104b545dd444b17f780e
Adam Horrigan <adhorrig@gmail.com>
2015-11-11 19:47:08 +0000

    Controllers and models improved

---
+++ TameImpala/Models/TameImpalaContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TameImpala.Models
+{
+    public class TameImpalaContext : DbContext
+    {
+        public TameImpalaContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<FavoriteGig> FavoriteGigs { get; set; }
+        public DbSet<UpcomingGig> UpcomingGigs { get; set; }
+        public DbSet<PreviousGig> PreviousGigs { get; set; }
+        public DbSet<GoingToGig> GoingToGigs { get; set; }
+    }
+}
