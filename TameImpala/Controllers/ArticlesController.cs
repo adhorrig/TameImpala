e48fd7d2f1908385bce29c4dcf0fb7652aff78e7
Adam Horrigan <adhorrig@gmail.com>
2015-11-28 23:40:28 +0000

    Shorten URL lengths using bitly API

--- TameImpala/Controllers/ArticlesController.cs
+++ TameImpala/Controllers/ArticlesController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 using TameImpala.Models;
 
 namespace TameImpala.Controllers
@@ -48,8 +51,38 @@ namespace TameImpala.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ArticleID,URL,Title,Description")] Article article)
         {
+
+            string shortUrl = string.Empty;
+            string urlToShorten = article.URL;
+            XmlDocument xmlDoc = new XmlDocument();
+
+            WebRequest request = WebRequest.Create("http://api.bitly.com/v3/shorten");
+            byte[] data = Encoding.UTF8.GetBytes(string.Format("login={0}&apiKey={1}&longUrl={2}&format={3}",
+                "adhorrig",
+                "R_ab2d9f1a9098449c9300e3ab98872329",
+                HttpUtility.UrlEncode(urlToShorten),
+                "xml"));
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+            using(Stream ds = request.GetRequestStream())
+            {
+                ds.Write(data, 0, data.Length);
+            }
+            using(WebResponse response = request.GetResponse())
+            {
+                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                {
+                    xmlDoc.LoadXml(sr.ReadToEnd());
+                }
+            }
+            shortUrl = xmlDoc.GetElementsByTagName("url")[0].InnerText;
+            
+
             if (ModelState.IsValid)
             {
+                article.URL = shortUrl;
                 db.Articles.Add(article);
                 db.SaveChanges();
                 return RedirectToAction("Index");
